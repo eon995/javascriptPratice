fae9d1e9c4e982c37288dc8ee481cc0581c8cfa5
eon995 <84843589+eon995@users.noreply.github.com>
2022-01-01 15:45:59 +0900

    crowngame

---
+++ 3_control/game.js
@@ -0,0 +1,15 @@
+let funds = 50;
+
+while (funds > 1 && funds < 100) {
+    // 돈을 겁니다.
+
+    //주사위를 굴립니다.
+
+    //맞췄으면 돈을 가집니다.
+
+    funds = funds + 2;
+    console.log(funds);
+    funds = funds - 1;
+    console.log(funds);
+
+}
